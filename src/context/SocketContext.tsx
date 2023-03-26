e2b5f156dc52af9886cf764955685efabeda7a8b
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-03-26 23:39:57 +0800

    * added socketio server connection

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -1,8 +1,8 @@
 import { createContext, useContext } from "react";
 import io, { Socket } from "socket.io-client";
 
-// const socket = io('http://localhost:3000', { transports: ['websocket'] });
-const socket = io();
+const socket = io("https://viral-isolation-socketio.fly.dev/", { transports: ["websocket"] });
+// const socket = io();
 socket.on("connect", () => {
   console.log("Connected to server");
 });
