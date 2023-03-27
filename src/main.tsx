20b0236de939a95a99e77c4d9f1bee36994e8a12
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-03-27 20:36:06 +0800

    * removed Rollbar code in main

--- src/main.tsx
+++ src/main.tsx
@@ -4,27 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-// import { Provider, ErrorBoundary } from "@rollbar/react";
-
-// const rollbarConfig = {
-//   accessToken: 'b72b1a50553344729791314f58479ae8',
-//   environment: 'testenv',
-// };
-
-// function TestError() {
-//   const a = null;
-//   return a.hello();
-// }
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    {/* <Provider config={rollbarConfig}>
-      <ErrorBoundary> */}
     <BrowserRouter>
       <App />
-      {/* <TestError /> */}
     </BrowserRouter>
-    {/* </ErrorBoundary>
-    </Provider> */}
   </React.StrictMode>,
 );
