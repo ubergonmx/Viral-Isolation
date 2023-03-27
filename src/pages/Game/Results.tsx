ae0911505b5c9364797534ab616b64ed090dd7b8
JohnMarcSevillana <john_marc_sevillana@dlsu.edu.ph>
2023-03-27 23:57:26 +0800

    Added result values and a few designs

--- src/pages/Game/Results.tsx
+++ src/pages/Game/Results.tsx
@@ -22,14 +22,31 @@ function Results({ results }: { results: GameResult }) {
   return (
     <>
       <h1>Results</h1>
+      <fieldset style={{ textAlign: "left", backgroundColor: "yellow" }}>
+        <legend>Game Information:</legend>
+        <p>Game ID: {results.id}</p>
+        <p>Game Time: {results.gameTime}</p>
+      </fieldset>
       <div>
         {results.survivor.map((survivor, index) => (
           <div key={index}>
-            <p>display details</p>
+            <fieldset style={{ textAlign: "left", backgroundColor: "green" }}>
+              <legend>{survivor.name}</legend>
+              <p>HP: {survivor.hp}</p>
+              <p>Has Escaped: {String(survivor.hasEscaped)}</p>
+              <p>Is Dead: {String(survivor.isDead)}</p>
+            </fieldset>
           </div>
         ))}
       </div>
-      <div>{/* viral */}</div>
+      <div>
+        <fieldset style={{ textAlign: "left", backgroundColor: "red" }}>
+          <legend>{results.viral.name}</legend>
+          <p>Skill: {results.viral.skill}</p>
+          <p>Number of Infections: {results.viral.noOfInfections}</p>
+          <p>Number of Killings: {results.viral.noOfKillings}</p>
+        </fieldset>
+      </div>
     </>
   );
 }
